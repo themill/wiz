3b425485952a28094eea5ab3095986ad098699d1
Jeremy Retailleau <jeremyr@themill.com>
2017-12-06 21:10:46 -0800

    Mutate the definition instead of returning teh augmented version

--- source/umwelt/command_line.py
+++ source/umwelt/command_line.py
@@ -169,9 +169,7 @@ def main(arguments=None):
             registries, max_depth=namespace.definition_search_depth
         )
         definition = umwelt.definition.get(namespace.definition, mapping)
-        definition = umwelt.definition.resolve_dependencies(
-            definition, mapping
-        )
+        umwelt.definition.resolve_dependencies(definition, mapping)
 
 
 def local_registry():
