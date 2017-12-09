47603c9d444740d38d5612581837f27a35cbae5b
Jeremy Retailleau <jeremyr@themill.com>
2017-12-08 18:48:34 -0800

    Improve graph construction and implement sorting and environment merging

--- source/umwelt/environment.py
+++ source/umwelt/environment.py
@@ -1,19 +1,24 @@
 # :coding: utf-8
 
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
+
 import mlog
 import collections
 
 import umwelt.definition
 
 
-#: Environment leaf composing the :class:`EnvironmentTree`.
-Environment = collections.namedtuple(
-    "Environment", ["identifier", "definition", "requirement", "parent"]
+#: Node leaf composing the :class:`DependencyTree`.
+Node = collections.namedtuple(
+    "Node", ["identifier", "definition", "requirement", "parent"]
 )
 
 
-def create_tree(requirements, definition_mapping):
-    """Return the environment dependency tree corresponding to *requirements*.
+def resolve(requirements, definition_mapping):
+    """Return resolved environment mapping corresponding to *requirements*.
 
     *requirements* indicates a list of definition requirements which must
     adhere to `PEP 508 <https://www.python.org/dev/peps/pep-0508>`_.
@@ -30,37 +35,135 @@ def create_tree(requirements, definition_mapping):
     """
     logger = mlog.Logger(__name__ + ".create_tree")
     logger.info(
-        "Build dependency tree: {!r}".format(requirements)
+        "Resolve environment: {!r}".format(requirements)
     )
 
-    tree = EnvironmentTree()
-
+    graph = Graph()
     for requirement in requirements:
-        tree.add_branch(requirement, definition_mapping)
+        graph.add_node(requirement, definition_mapping)
 
-    return tree
+    definitions = sorted_definitions(graph)
+
+    return reduce(
+        lambda def1, def2: combined_environment(def1, def2), definitions
+    )
 
 
-class EnvironmentTree(object):
-    """Environment Tree.
+def sorted_definitions(graph):
+    """Return list of definitions topologically sorted from *graph*.
+    """
+    queue = Queue()
+    indegree_mapping = dict()
 
-    Represent a directed acyclic graph containing all dependent environment
-    mappings.
+    for identifier in graph.identifiers:
+        indegree_mapping[identifier] = graph.indegree(identifier)
 
+        # Queue all node identifiers which have no parent
+        if indegree_mapping[identifier] == 0:
+            queue.put(identifier)
+
+    definitions = []
+
+    while not queue.empty():
+        identifier = queue.get()
+
+        definitions.append(
+            graph.definition(identifier)
+        )
+
+        for identifier in graph.adjacents(identifier):
+            indegree_mapping[identifier] = indegree_mapping[identifier] - 1
+
+            if indegree_mapping[identifier] == 0:
+                queue.put(identifier)
+
+    if len(definitions) != len(graph.identifiers):
+        raise RuntimeError(
+            "A cycle has been detected in the dependency graph"
+        )
+
+    return definitions
+
+
+def combined_environment(definition1, definition2):
+    """Return combined data environment from *definition1* and *definition2*"""
+    environment = dict()
+
+    # Extract environment from definitions
+    environment1 = definition1.get("data", {})
+    environment2 = definition2.get("data", {})
+
+    for key, value2 in environment2.items():
+        value1 = environment1.get(key)
+
+        # The keyword must not have a value in the two environment, unless if
+        # it is a list that can be combined.
+        if value1 is not None:
+            if not isinstance(value1, list):
+                raise RuntimeError(
+                    "Overriding environment variable per definition is "
+                    "forbidden\n"
+                    " - {key}={value1} [{definition1}]\n"
+                    " - {key}={value2} [{definition2}]\n".format(
+                        key=key, value1=value1, value2=value2,
+                        definition1=definition1.identifier,
+                        definition2=definition2.identifier,
+                    )
+                )
+
+            environment[key] = value1 + value2
+
+        # Otherwise simply set the value2
+        else:
+            environment[key] = value2
+
+    return environment
+
+
+class Graph(object):
+    """Dependency Graph.
     """
 
     def __init__(self):
-        """Initialise graph."""
+        """Initialise Graph."""
         self._logger = mlog.Logger(__name__ + ".EnvironmentGraph")
 
         # Record the weight of each link in the graph.
         self._links = dict()
 
-        # Record environments used per identifier.
-        self._environments = dict()
+        # Record node leaf created per definition identifier.
+        self._nodes = dict()
 
-    def add_branch(self, requirement, definition_mapping, parent=None):
-        """Recursively create branch from *definition_requirement*.
+    @property
+    def identifiers(self):
+        """Return all environment identifiers in the tree."""
+        return self._nodes.keys()
+
+    def definition(self, identifier):
+        """Return definition from *identifier*."""
+        return self._nodes[identifier].definition
+
+    def indegree(self, identifier):
+        """Return indegree value for *identifier*."""
+        indegree = 0
+
+        for parent_identifier in self._nodes.keys():
+            if self._links.get(parent_identifier, {}).get(identifier, 0) > 0:
+                indegree = indegree + 1
+
+        return indegree
+
+    def adjacents(self, identifier):
+        """Return list of adjacent node identifiers for *identifier*."""
+        identifiers = []
+        for _identifier in self._nodes.keys():
+            if self._links.get(identifier, {}).get(_identifier, 0) > 0:
+                identifiers.append(_identifier)
+
+        return identifiers
+
+    def add_node(self, requirement, definition_mapping, parent=None):
+        """Recursively create dependency node(s) from *definition_requirement*.
 
         *requirement* indicates a definition requirement which must
         adhere to `PEP 508 <https://www.python.org/dev/peps/pep-0508>`_.
@@ -74,67 +177,71 @@ class EnvironmentTree(object):
         Raise :exc:`RuntimeError` is a version conflict is detected.
 
         """
-        self._logger.debug("Create branch for {}".format(requirement))
+        self._logger.debug("Add node for '{}'".format(requirement))
 
         definition = umwelt.definition.get(requirement, definition_mapping)
 
-        # Create environment from definition.
-        environment = Environment(
-            definition.identifier, definition, requirement, parent
-        )
+        # Create node from definition.
+        node = Node(definition.identifier, definition, requirement, parent)
 
-        self.check_conflict(environment)
+        # Check is a node with the same definition identifier is in the graph.
+        if node.identifier in self._nodes.keys():
+            _node = self._nodes[node.identifier]
 
-        # Record environments.
-        self._environments.setdefault(definition.identifier, [])
-        self._environments[definition.identifier].append(environment)
+            # If the definition identifier has already been used in the graph
+            # with a specifier incompatible with the current definition's
+            # version, raise an error.
+            if node.definition.version not in _node.requirement.specifier:
+                raise RuntimeError(
+                    "A version conflict has been detected for '{id!r}'\n"
+                    " - {node1}\n"
+                    " - {node2}\n".format(
+                        id=node.requirement,
+                        node1=self._display_node(node),
+                        node2=self._display_node(_node),
+                    )
+                )
 
-        # Add a dependency link if necessary
+            # Otherwise update the node with the highest version if necessary.
+            updated = max(node, _node, key=lambda n: n.definition.version)
+
+            if updated.definition.version != _node.requirement.specifier:
+                self._nodes[node.identifier] = updated
+
+                self._logger.debug(
+                    "Replace {identifier!r}: {version1} -> {version2}".format(
+                        identifier=node.identifier,
+                        version1=_node.definition.version,
+                        version2=updated.definition.version
+                    )
+                )
+
+        # Add node to the graph if no other node with the same definition
+        # identifier is found.
+        else:
+            self._nodes[node.identifier] = node
+            self._logger.debug(
+                "Create {0.identifier!r}: {0.definition.version}".format(
+                    self._nodes[node.identifier])
+            )
+
+        # Add a dependency link if necessary.
         if parent is not None:
-            self.add_link(environment.identifier, parent.identifier)
+            self.add_link(node.identifier, parent.identifier)
 
-        # Recursively resolve all dependencies
+        # Recursively resolve all dependencies.
         dependencies = definition.get("dependency", [])
         for dependency_requirement in dependencies:
-            self.add_branch(
+            self.add_node(
                 dependency_requirement, definition_mapping, parent=definition
             )
 
-    def check_conflict(self, environment):
-        """Ensure that *environment* is not conflicting with other environments.
+    def _display_node(self, node):
+        """Return formatted identifier for :class:`Node` instance*."""
+        message = "{}".format(node.requirement)
 
-        *environment* is a :class:`Environment` instance.
-
-        Raise :exc:`RuntimeError` is a conflict is detected.
-
-        """
-        if environment.identifier not in self._environments.keys():
-            return
-
-        conflicting_environment = None
-
-        for _environment in self._environments[environment.identifier]:
-            if _environment.version not in _environment.requirement.specifier:
-                conflicting_environment = _environment
-                break
-
-        if conflicting_environment is not None:
-            raise RuntimeError(
-                "A version conflict has been detected for '{id!r}'\n"
-                " - {env1}\n"
-                " - {env2}\n".format(
-                    id=environment.requirement,
-                    env1=self._display(environment),
-                    env2=self._display(conflicting_environment),
-                )
-            )
-
-    def _display(self, environment):
-        """Return formatted identifier for :class:`Environment` instance*."""
-        message = "{}".format(environment.requirement)
-
-        if environment.parent is not None:
-            message += " [from {!r}]".format(environment.parent.identifier)
+        if node.parent is not None:
+            message += " [from {!r}]".format(node.parent.identifier)
 
         return message
 
@@ -154,6 +261,12 @@ class EnvironmentTree(object):
         set for this *parent*.
 
         """
+        self._logger.debug(
+            "Add dependency link from {parent!r} to {child!r}".format(
+                parent=parent_identifier, child=identifier
+            )
+        )
+
         self._links.setdefault(parent_identifier, {})
 
         if identifier in self._links[parent_identifier].keys():
