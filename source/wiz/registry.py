7d46c0b6a98ca28c9e988e68f158470c24813fd0
jeremyr <jeremyr@themill.com>
2018-10-18 18:56:08 -0700

    Improve docstring and variable names

--- source/wiz/registry.py
+++ source/wiz/registry.py
@@ -91,16 +91,18 @@ def discover(path):
             yield registry_path
 
 
-def install_to_path(definition, registry_path, hierarchy=None, overwrite=False):
+def install_to_path(
+    definition, registry_path, hierarchy_list=None, overwrite=False
+):
     """Install a definition to a registry on the file system.
 
     *definition* must be a valid :class:`~wiz.definition.Definition` instance.
 
-    *registry_path* is the target registry path to install to (ending on
-    `.wiz/.registry`).
+    *registry_path* is the target registry path to install to.
 
-    *hierarchy* within the target registry to install the definition to. If not
-    specified, it will be installed in the root of the registry.
+    *hierarchy_list* could be a list of sub-folders within the registry to
+    install the definition to. By default the definition will be installed in
+    the root of the registry.
 
     If *overwrite* is True, any existing definitions in the target registry
     will be overwritten.
@@ -129,8 +131,8 @@ def install_to_path(definition, registry_path, hierarchy=None, overwrite=False):
     if not registry_path.endswith(".wiz/registry"):
         registry_path = os.path.join(registry_path, ".wiz", "registry")
 
-    if hierarchy is not None:
-        registry_path = os.path.join(registry_path, *hierarchy)
+    if hierarchy_list is not None:
+        registry_path = os.path.join(registry_path, *hierarchy_list)
 
     try:
         wiz.export_definition(registry_path, definition, overwrite=overwrite)
@@ -154,17 +156,18 @@ def install_to_path(definition, registry_path, hierarchy=None, overwrite=False):
 
 
 def install_to_vault(
-    definition, registry_identifier, hierarchy=None, overwrite=False
+    definition, registry_identifier, hierarchy_list=None, overwrite=False
 ):
     """Install a definition to a repository registry.
 
     *definition* must be a valid :class:`~wiz.definition.Definition` instance.
 
-    *registry_identifier* is the identifier of the target registry repository to
-    install to.
+    *registry_identifier* is the identifier of the target :term:`Wiz Vault`
+    registry to install to.
 
-    *hierarchy* within the target registry to install the definition to. If not
-    specified, it will be installed in the root of the registry.
+    *hierarchy_list* could be a list of sub-folders within the registry to
+    install the definition to. By default the definition will be installed in
+    the root of the registry.
 
     If *overwrite* is True, any existing definitions in the target registry
     will be overwritten.
@@ -203,7 +206,7 @@ def install_to_vault(
         params={"overwrite": json.dumps(overwrite)},
         data={
             "content": definition.encode(),
-            "hierarchy": json.dumps(hierarchy or []),
+            "hierarchy": json.dumps(hierarchy_list or []),
             "message": (
                 "Add {identifier!r} [{version}] to registry ({username})"
                 "\n\nauthor: {name}".format(
