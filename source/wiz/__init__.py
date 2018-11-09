017a90da7045d4fb4fdcb2b0c65a1e42244aaa99
jeremyr <jeremyr@themill.com>
2018-11-09 14:16:15 -0800

    Remove request option from top level api function

--- source/wiz/__init__.py
+++ source/wiz/__init__.py
@@ -16,9 +16,7 @@ import wiz.exception
 import wiz.utility
 
 
-def fetch_definition_mapping(
-    paths, requests=None, max_depth=None, system_mapping=None
-):
+def fetch_definition_mapping(paths, max_depth=None, system_mapping=None):
     """Return mapping from all definitions available under *paths*.
 
     Discover all available definitions under *paths*, searching recursively
@@ -50,10 +48,6 @@ def fetch_definition_mapping(
             ]
         }
 
-    *requests* could be a list of element which can influence the definition
-    research. It can be in the form of "package >= 1.0.0, < 2" in order to
-    affine the research to a particular version range.
-
     *system_mapping* could be a mapping of the current system. By default, the
     current system mapping will be :func:`queried <wiz.system.query>`.
 
@@ -62,10 +56,7 @@ def fetch_definition_mapping(
         system_mapping = wiz.system.query()
 
     mapping = wiz.definition.fetch(
-        paths,
-        requests=requests,
-        system_mapping=system_mapping,
-        max_depth=max_depth
+        paths, system_mapping=system_mapping, max_depth=max_depth
     )
 
     mapping["registries"] = paths
