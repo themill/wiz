f86603d9430f4f022c32ab43e3f4ce9eaee50f6c
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2020-08-04 09:00:31 -0700

    Update installation command to pass an extended TOML config when installing Wiz

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # :coding: utf-8
 
+import collections
 import os
 import re
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 RESOURCE_PATH = os.path.join(ROOT_PATH, "resource")
@@ -19,6 +21,11 @@ with open(
     VERSION = re.match(
         r".*__version__ = \"(.*?)\"", _version_file.read(), re.DOTALL
     ).group(1)
+
+# Package required for the installation setup script to run.
+SETUP_REQUIRES = [
+    "toml >= 0.10.1, < 1"
+]
 
 # Compute dependencies.
 INSTALL_REQUIRES = [
@@ -48,6 +55,68 @@ TEST_REQUIRES = [
 ]
 
 
+class InstallExtended(install):
+    """Custom install command to set Wiz config."""
+
+    # Extended options
+    user_options = install.user_options + [
+        ("wiz-config=", None, "TOML file to use as Wiz configuration.")
+    ]
+
+    # Path to initial Wiz config.
+    wiz_config = None
+
+    def finalize_options(self):
+        """Sanitise optional argument to run installation command."""
+        if self.wiz_config is not None:
+            # Load configuration mapping from TOML file path.
+            self.wiz_config = self._load_config(self.wiz_config)
+
+        install.finalize_options(self)
+
+    def run(self):
+        """Run installation command."""
+        import toml
+
+        if self.wiz_config is not None:
+            build_path = os.path.join(self.build_lib, PACKAGE_NAME)
+            path = os.path.join(build_path, "package_data", "config.toml")
+            data = self._load_config(path)
+
+            # Update configuration data mapping with new config.
+            data = self._deep_update(data, self.wiz_config)
+
+            # Overwrite configuration file previously built.
+            with open(path, "w") as stream:
+                toml.dump(data, stream)
+
+        install.run(self)
+
+    def _deep_update(self, mapping1, mapping2):
+        """Recursively update *mapping1* from *mapping2*."""
+        for key, value in mapping2.items():
+            if isinstance(value, collections.Mapping):
+                mapping1[key] = self._deep_update(mapping1.get(key, {}), value)
+            else:
+                mapping1[key] = value
+        return mapping1
+
+    @staticmethod
+    def _load_config(path):
+        """Load configuration mapping from TOML file *path*."""
+        import toml
+
+        try:
+            with open(path, "r") as stream:
+                return toml.load(stream)
+
+        except Exception:
+            raise ValueError(
+                "The Wiz configuration cannot be loaded from path: {}"
+                .format(os.path.abspath(path))
+            )
+
+
 setup(
     name="wiz",
     version=VERSION,
@@ -61,6 +130,7 @@ setup(
         "": "source"
     },
     include_package_data=True,
+    setup_requires=SETUP_REQUIRES,
     install_requires=INSTALL_REQUIRES,
     tests_require=TEST_REQUIRES,
     extras_require={
@@ -73,5 +143,8 @@ setup(
         "console_scripts": [
             "wiz = wiz.__main__:main"
         ]
+    },
+    cmdclass={
+        "install": InstallExtended,
     },
 )
