14f3867faa6beaa8fcc0cddbdd6e98b05625e698
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2020-08-26 15:02:48 -0700

    Add classifiers

--- setup.py
+++ setup.py
@@ -119,4 +119,17 @@ setup(
     cmdclass={
         "build_py": BuildExtended,
     },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 "
+        "(LGPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+    ],
 )
